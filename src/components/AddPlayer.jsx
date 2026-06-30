4ef67a3b53a0a27a40a797a568dd8111283d6979
milesMejala <emstevenpatrick@gmail.com>
2026-07-01 00:47:02 +0800

    feat: implement AddPlayer component for roster management

--- src/components/AddPlayer.jsx
+++ src/components/AddPlayer.jsx
@@ -1,10 +1,41 @@
+import { useState } from "react";
 
 export default function AddPlayer() {
-    return (
-        <div>
-            <form action="/submit" method="post">
-                
-            </form>
-        </div>
-    )
+  const [formData, setFormData] = useState({
+    name: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+
+    setFormData({
+      name: "",
+    });
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          value={formData.name}
+          onChange={handleChange}
+          placeholder="Player Name"
+        />
+        <button type="submit">Add to Roster</button>
+      </form>
+    </div>
+  );
 }
